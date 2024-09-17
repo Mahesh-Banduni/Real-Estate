3f47ad98830aa2b42e4d0c362c24e2609cb1967a
Mahesh-Banduni <maheshbanduni9997@gmail.com>
2024-09-17 10:56:32 +0530

    code updated

---
+++ backend/src/controllers/Residential/penthouseController.js
@@ -0,0 +1,51 @@
+const penthouseService = require("../../services/Residential/penthouseService");
+
+// Create a new Penthouse
+exports.createPenthouse = async (req, res, next) => {
+  try {
+    const penthouse = await penthouseService.createPenthouse(req.body);
+    res.status(201).json(penthouse);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get all Penthouses
+exports.getAllPenthouses = async (req, res, next) => {
+  try {
+    const penthouses = await penthouseService.getAllPenthouses();
+    res.status(200).json(penthouses);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get Penthouse by ID
+exports.getPenthouseById = async (req, res, next) => {
+  try {
+    const penthouse = await penthouseService.getPenthouseById(req.params.id);
+    res.status(200).json(penthouse);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Update Penthouse by ID
+exports.updatePenthouse = async (req, res, next) => {
+  try {
+    const updatedPenthouse = await penthouseService.updatePenthouse(req.params.id, req.body);
+    res.status(200).json(updatedPenthouse);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Delete Penthouse by ID
+exports.deletePenthouse = async (req, res, next) => {
+  try {
+    await penthouseService.deletePenthouse(req.params.id);
+    res.status(200).json({ message: 'Penthouse deleted successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
