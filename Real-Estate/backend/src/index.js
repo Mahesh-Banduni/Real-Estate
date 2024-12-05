0793ee73d4fe543e80935fb7408151702d8e44d1
root <root@ubuntu-cloudpanel.localhost>
2024-12-05 11:08:07 +0000

    removed duplicate files

--- Real-Estate/backend/src/index.js
+++
@@ -1,4 +0,0 @@
-const clusterConfig = require('./configs/cluster.config.js');
-
-// Start the clustering and worker process management
-clusterConfig();
--- Real-Estate/backend/src/models/amenities.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const amenitiesSchema = new mongoose.Schema({
-  value: { type: String, required: true },
-  label: { type: String, required: true },
-});
-
-module.exports = amenitiesSchema;
